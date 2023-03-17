a70ba9996cb81207373af56094efec8e9402c2ea
yessid_vargas <yevasa93@gmail.com>
2023-03-16 22:25:08 -0500

    API comentarios con Fetch

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Comentario from './Components/Comentario'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Comentario></Comentario>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Comentario.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+
+const Comentario = () => {
+    const [data, setdata] = useState([])
+    const [comentario, setComentario] = useState({
+        id: 0,
+        texto:'',
+        boton:false
+    })
+
+    const url = 'https://jsonplaceholder.typicode.com/comments'
+
+    //fetch dentro de use effect para que lo haga una sola vez
+    //antes de el setdata hacer console log para ver que contiene
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setdata(data))     
+    }, [])
+
+    //el if lo hago porque al inicio data esta vacio y asi evito que no me
+    // saque error por tratar de ingresar a un array null
+    useEffect(() => {
+        if (data.length > 1) {
+            setComentario({...comentario, texto: data[comentario.id].body})
+        }
+    },[comentario.boton])
+
+
+    //console.log(data[0])
+    return (
+        <div>
+            <h1>Mesa Con API de Comentarios</h1>
+            <label>escriba unnumero entre 0-500</label>
+            <input type='number' min='1' max='500' onChange={
+                (e) => setComentario({...comentario, id: e.target.value})
+                }></input>
+            <button onClick={
+                () => setComentario({...comentario, boton: !(comentario.boton)})
+                }> BUSCAR </button>
+            <p>{comentario.texto}</p>
+        </div>
+    )
+}
+
+export default Comentario
